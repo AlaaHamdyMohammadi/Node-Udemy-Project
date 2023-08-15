ca26901cd34d74b37a6a8df531479392f751b6a3
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-15 23:46:17 +0200

    GetMethod

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
 const User = require('./../models/userModel');
 
-exports.getAllUsers = (req, res) => {
-  res.status(200).json({
-    status: "Success",
-    data,
-  });
+exports.getAllUsers = async(req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json({
+            status: "Success",
+            data: {
+                users,
+            }
+        });
+    }catch(err){
+        res.status(400).json({
+          status: "Faild",
+          message: err,
+        });
+    }
 };
 
 exports.createUser = async(req, res) => {
@@ -23,21 +33,22 @@ exports.createUser = async(req, res) => {
     }
 };
 
-exports.getUser = (req, res) => {
-  // const id = req.params.id * 1;
-  //const user =
-
-  // if(id > users.length){
-  //   return res.status(404).json({
-  //     status: 'Faild',
-  //     message: 'Invalid ID',
-  //   })
-  // }
-
-  res.status(200).json({
+exports.getUser = async(req, res) => {
+  try{
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
     status: "Success",
-    data,
-  });
+    data: {
+        user
+    }
+    });
+  }catch(err){
+    res.status(400).json({
+      status: "Faild",
+      message: err,
+    });
+  }
+
   // console.log(req.body);
 };
 
