de61bbb20e8a47b1977f33fcb688b531fb6dce49
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-15 23:57:11 +0200

    PatchMethod

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,48 @@
-const User = require('./../models/userModel');
+const User = require("./../models/userModel");
 
-exports.getAllUsers = async(req, res) => {
-    try{
-        const users = await User.find();
-        res.status(200).json({
-            status: "Success",
-            data: {
-                users,
-            }
-        });
-    }catch(err){
-        res.status(400).json({
-          status: "Faild",
-          message: err,
-        });
-    }
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "Success",
+      data: {
+        users,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Faild",
+      message: err,
+    });
+  }
 };
 
-exports.createUser = async(req, res) => {
-    try{
-      // console.log(req.body);
-      const newUser = await User.create(req.body);
-      res.status(201).json({
-        status: "Success",
-        data: newUser,
-      });
-    }catch(err){
-        res.status(400).json({
-            status: "Faild",
-            message: err,
-        });
-    }
+exports.createUser = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: "Success",
+      data: newUser,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Faild",
+      message: err,
+    });
+  }
 };
 
-exports.getUser = async(req, res) => {
-  try{
+exports.getUser = async (req, res) => {
+  try {
     const user = await User.findById(req.params.id);
     res.status(200).json({
-    status: "Success",
-    data: {
-        user
-    }
+      status: "Success",
+      data: {
+        user,
+      },
     });
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
       status: "Faild",
       message: err,
@@ -52,19 +52,21 @@ exports.getUser = async(req, res) => {
   // console.log(req.body);
 };
 
-exports.updateUser = (req, res) => {
-  //const id = req.params.id * 1;
-  // if(id > users.length){
-  //   return res.status(404).json({
-  //     status: 'Faild',
-  //     message: 'Invalid ID',
-  //   })
-  // }
-
-  res.status(200).json({
-    status: "Success",
-    data,
-  });
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    res.status(200).json({
+      status: "Success",
+      data:{
+        user
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Faild",
+      message: err,
+    });
+  }
 };
 
 exports.deleteUser = (req, res) => {
