93a9708ecae0b672fc0a1fb3b8c7681cb9a7c4d1
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-17 01:25:28 +0200

    SignUpMethod

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,22 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: [true, "Username is required!"] },
-    email: { type: String, required: [true, "Email is required!"] },
-    password: { type: String, required: [true, "Password is required!"] },
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: [true, "Username is required!"] },
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, 'Please write a valid email'],
   },
-  { timestamps: true }
-);
+  password: { type: String, required: [true, "Please provide a password!"], minlength: 5 },
+  passwordConfirm: { type: String, required: [true, "Please confirm your password!"] },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
