298286ee37ad1b13d7f23955d65e9b316a5dd02f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-18 00:30:55 +0200

    Encrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,46 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: [true, "Username is required!"] },
+  username: { type: String,required: [true, "Username is required!"] },
   email: {
     type: String,
     required: [true, "Email is required!"],
     unique: true,
     lowercase: true,
-    validator: [validator.isEmail, 'Please write a valid email'],
+    validator: [validator.isEmail, "Please write a valid email"],
   },
-  password: { type: String, required: [true, "Please provide a password!"], minlength: 5 },
-  passwordConfirm: { type: String, required: [true, "Please confirm your password!"] },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    minlength: 5,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password!"],
+    validate: {
+      //Work on create user and save
+      validator: function (value) {
+        return value === this.password;
+      },
+      message: "Passwords are not the same!",
+    },
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+userSchema.pre('save', async function(next){
+  //Encrypt password if password field is updated
+  if(!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //After check delete passwordConfirm from db
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
