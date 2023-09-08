8d0b2701f9f2aaf64458a7182196d18c8d29b548
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-09-09 01:18:28 +0200

    AddCategoryURL

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const userRouter = require('./routes/userRoutes');
 const courseRouter = require('./routes/courseRoutes');
 const reviewRouter = require("./routes/reviewRoutes");
+const categoryRouter = require('./routes/categoryRoutes');
 const buyingRouter = require("./routes/buyingRoutes");
 const mongoose = require('mongoose');
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/courses", courseRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/buyings", buyingRouter);
 
 
