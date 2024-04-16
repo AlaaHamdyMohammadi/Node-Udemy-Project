5ee80c30f07963a8302bf1a123a7bf2491f5656f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-04-16 16:44:17 +0200

    Update links

---
+++ courseRoutes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const courseController = require("./controllers/courseController");
+const authController = require("./controllers/authController");
+const reviewRoutes = require('./routes/reviewRoutes');
+
+const courseRouter = express.Router({ mergeParams: true });
+
+courseRouter.use("/:courseId/reviews", reviewRoutes);
+
+courseRouter
+  .route("/")
+  .get(
+    // authController.protect,
+    // authController.restrictTo("instructor", "admin"),
+    courseController.getAllCourse
+  )
+  .post(
+    authController.protect,
+    authController.restrictTo("instructor", "admin"),
+    courseController.uploadCoursePhoto,
+    courseController.resizeCoursePhoto,
+    courseController.createCourse
+  );   
+courseRouter
+  .route("/:id")
+  .get(courseController.getCourse)
+  .patch(
+    courseController.uploadCoursePhoto,
+    courseController.resizeCoursePhoto,
+    courseController.updateCourse
+  ) //authController.protect, authController.restrictTo("instructor", "admin"),
+  .delete(
+    authController.protect,
+    authController.restrictTo("instructor", "admin"), //passed role that allawed to interact with this resource (return middleware)
+    courseController.deleteCourse
+  );
+
+module.exports = courseRouter;
